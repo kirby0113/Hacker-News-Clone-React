0f74f5dd19c02cc4f584db8760c6b16167c3d332
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-12-24 21:35:32 +0900

    refactor: Color.jsに合わせてコード修正

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -3,14 +3,15 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import { FontFamily, FontSize } from "../constants/Font";
+import { Color } from "../constants/Color";
 
 const StyledNewsCard = styled.div`
   display: grid;
   grid-template-columns: max-content 1fr;
-  border: solid 2px #263238;
+  border: solid 2px ${Color["black"]};
   border-radius: 20px;
   align-items: center;
-  background-color: #f5f5f5;
+  background-color: ${Color["lightgrey"]};
   box-shadow: 3px 3px 4px #00000040;
 `;
 
@@ -27,6 +28,7 @@ const ScoreTextTop = styled.span`
   font-weight: 700;
   font-size: ${FontSize["xLarge"]};
   letter-spacing: 1.5px;
+  color: ${Color["black"]};
 `;
 
 const ScoreTextBottom = styled.span`
@@ -35,6 +37,7 @@ const ScoreTextBottom = styled.span`
   font-size: ${FontSize["small"]};
   letter-spacing: 3px;
   padding-bottom: 13px;
+  color: ${Color["black"]};
 `;
 
 const NewsCardDetail = styled.div`
@@ -49,13 +52,13 @@ const NewsCardTitle = styled.span`
   font-weight: 500;
   font-size: ${FontSize["medium"]};
   letter-spacing: 2px;
-  color: #ff5722;
+  color: ${Color["primary"]};
 `;
 
 const NewsCardLink = styled.a`
   margin-top: 2px;
   font-family: ${FontFamily["Primary"]}, serif;
-  color: #666666;
+  color: ${Color["grey"]};
   font-weight: 500;
   font-size: ${FontSize["xSmall"]};
   text-decoration: none;
@@ -73,16 +76,19 @@ const NewsCardAuthor = styled.span`
   font-family: ${FontFamily["Primary"]}, serif;
   font-weight: 400;
   font-size: ${FontSize["xSmall"]};
+  color: ${Color["black"]};
 `;
 
 const NewsCardComment = styled.span`
   font-family: ${FontFamily["Secondary"]}, sans-serif;
   font-size: ${FontSize["xSmall"]};
+  color: ${Color["black"]};
 `;
 
 const NewsCardDate = styled.span`
   font-family: ${FontFamily["Secondary"]}, , sans-serif;
   font-size: ${FontSize["xSmall"]};
+  color: ${Color["black"]};
 `;
 
 export const NewsCard = ({ NewsData }) => {
