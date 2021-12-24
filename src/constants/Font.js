ae686cd013dd88b5482732b49bb8caf8ec3813da
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-12-24 20:46:11 +0900

    feat: Font周りの定数用ファイルを作成

---
+++ src/constants/Font.js
@@ -0,0 +1,13 @@
+export const FontFamily = {
+  Logo: "Merriweather",
+  Primary: "Raleway",
+  Secondary: "Open Sans",
+};
+
+export const FontSize = {
+  xSmall: "1rem",
+  small: "1.3rem",
+  medium: "2rem",
+  large: "2.5rem",
+  xLarge: "3rem",
+};
